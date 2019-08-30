914e7fed44932c53eb39c543eac08317f53141cb
Maja Kuzman <37149324+MaKuzman@users.noreply.github.com>
2019-08-30 17:07:17 +0200

    Create checkPackages.R

---
+++ checkPackages.R
@@ -0,0 +1,12 @@
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+}
+
+# Usage example
+packages<-c("ggplot2", "rafalib", "data.table")
+check.packages(packages)
